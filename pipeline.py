2b0528f6aa26877a7d60026deb3a0f81f61bb894
Gabriel Simpson <gabe@gabrielsimpson.com>
2024-03-03 07:26:32 -0500

    Old Instructor Input/super pipeline

--- pipeline.py
+++ pipeline.py
@@ -2,23 +2,31 @@ import MathMe
 
 # entirety of the pipeline, calls instructorInput once, then pipeline for as many students as instructur inputs
 def superPipeline():
-    return
+    inInput = instructorInput().splitlines()
+    if(int(inInput[1]) < 0):
+        students = [int(inInput[3])][int(inInput[1])]
+        for i in range(0, int(inInput[3])):
+            students[i] = pipeline(inInput)
+        return students
+    else: 
+        return
 
 #runs through the pipeline for one student, called in superpipeline using returned value from instructorInput as parameters
-def pipeline():
-    return
-
-#temporary pipeline function that calls instructorInput in it, and assumes there is one student
-def tempPipeline():
+def pipeline(inInput):
+    
     return
 
 # asks instructor for input, returns multi-line string with the question, wanted number of steps, specific steps wanted, max numer of mistakes per step, and number of students
 def instructorInput():
-    return
-
-# takes student answer for one step and evaluates if it is a valid(will not cause error) response, will repeatedly ask until one is given and returns it.
-def studentInput():
-    return
+    print("----------------------------------------------------------------------------------------------------")
+    print("Instructor Input:")
+    question = input("Enter a math problem: ")
+    numSteps = input("Enter the number of steps the problem should take, enter 0 to leave it up to chatGPT: ")
+    specificSteps = input("List specific steps you want students to take in order in a comma separated list: ")
+    maxIncorrect = input("Enter the number of times a student can incorreclty answer a step: ")
+    print("----------------------------------------------------------------------------------------------------")
+    returnString = question + "\n" + numSteps + "\n" + specificSteps + "\n" + maxIncorrect
+    return returnString
 
 #does one loop of student answering and recieving feedback and returns if the student was correct
 def inputLoop():
