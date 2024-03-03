576bfa326f4eab74c353266ca377bc56d175302f
Gabriel Simpson <gabe@gabrielsimpson.com>
2024-03-02 23:07:11 -0500

    pipeline.py added

---
+++ pipeline.py
@@ -0,0 +1,25 @@
+import MathMe
+
+# entirety of the pipeline, calls instructorInput once, then pipeline for as many students as instructur inputs
+def superPipeline():
+    return
+
+#runs through the pipeline for one student, called in superpipeline using returned value from instructorInput as parameters
+def pipeline():
+    return
+
+#temporary pipeline function that calls instructorInput in it, and assumes there is one student
+def tempPipeline():
+    return
+
+# asks instructor for input, returns multi-line string with the question, wanted number of steps, specific steps wanted, max numer of mistakes per step, and number of students
+def instructorInput():
+    return
+
+# takes student answer for one step and evaluates if it is a valid(will not cause error) response, will repeatedly ask until one is given and returns it.
+def studentInput():
+    return
+
+#does one loop of student answering and recieving feedback and returns if the student was correct
+def inputLoop():
+    return
