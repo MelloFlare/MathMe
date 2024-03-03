91bbad27c74f38bda80c1b0b63f5ab1b1777c471
MelloFlare <dhirpatel91@gmail.com>
2024-03-03 07:25:05 -0500

    MOstly DOne

--- MathMe.py
+++ MathMe.py
@@ -36,7 +36,6 @@ def generate_message(questions):
 
 def print_response(response):
     content = response.choices[0].message.content
-    print(content)
     return content
 
 
@@ -44,8 +43,9 @@ def each_step(array):
     oldA = array
     newA = []
     for elemets in oldA:
-        if( elemets != ' ' and elemets[1] == "." or elemets[2] == '.'):
-            newA.append(elemets)
+        if( elemets != ''):
+           if(elemets[1] == "." or elemets[2] == '.'):
+                newA.append(elemets)
 
     return newA
 
@@ -58,25 +58,3 @@ def ask_step(steps):
         message = generate_message(questions)
         response = ask_question(message, model)
         print_response(response)
-
-
-#Setup = ["Use Step by Step format to answer future questions]
-questions = ["Say hi", """Solve the integral of 1/sin(x), and give me in steps, first substitute x = 2u , 
-             dx = 2du, then sin(2u) = 2sinucosu, then use the identity of 1 = sin(x)^2+cos(x)^2, , so that the integral become (sin(u)^2 + cos(u)^2 )/ sinu cosu, then reduced into two integrals, sinu/cosu and cosu/sinu, then let m = sinu , dm = cosu, and n = cosu , dn = -sinu, , the two integrals become 1/m and 1/n. then solve the integral to ln(m) - ln(n) +c , then use the identity of ln(m) - ln(n) = ln(m/n) +c. Then recall m = sinu, n = cosu, the equation become ln(sinu/cosu)+c which is lm(tanu)+c. Then recall x = 2u, so the final solution is ln(tan(x/2))+c Stay only on this approach, Keep each step in one line and number each step"""]
-#questions = ["what is (1 + 1)* 2 - 3 show each step"]
-messages = generate_message(questions)
-response = ask_question(messages, model)
-t = print_response(response)
-
-#questions = ["what math knowledge should the solver now for each step at that level?"]
-#message2 = generate_message(questions)
-#response2 = ask_question(message2, model)
-#print_response(response2)
-
-x = t.splitlines()
-print(x)
-y = each_step(x)
-test = "test"
-print(y)
-
-ask_step(y)
